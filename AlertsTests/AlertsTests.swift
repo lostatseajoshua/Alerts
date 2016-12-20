15704d308c7ff2f8066041b36bf0065448fd6b9b
Joshua Alvarado <alvaradojoshua0@gmail.com>
2016-12-20 13:26:38 -0700

    Add coordinator unit tests

--- AlertsTests/AlertsTests.swift
+++ AlertsTests/AlertsTests.swift
@@ -14,23 +14,46 @@ class AlertsTests: XCTestCase {
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        AlertCoordinator.main.reset()
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+        AlertCoordinator.main.reset()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testHighAlertQueue() {
+        let alert = Alert(title: "High", message: "Message", priorty: .high, alertActions: nil)
+        AlertCoordinator.main.enqueue(alert: alert)
+        XCTAssertEqual(AlertCoordinator.main.highPriorityQueue.count, 1)
+        XCTAssertEqual(AlertCoordinator.main.defaultPriorityQueue.count, 0)
+        XCTAssertEqual(AlertCoordinator.main.lowPriorityQueue.count, 0)
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testDefaultAlertQueue() {
+        let alert = Alert(title: "Default", message: "Message", priorty: .medium, alertActions: nil)
+        AlertCoordinator.main.enqueue(alert: alert)
+        XCTAssertEqual(AlertCoordinator.main.highPriorityQueue.count, 0)
+        XCTAssertEqual(AlertCoordinator.main.defaultPriorityQueue.count, 1)
+        XCTAssertEqual(AlertCoordinator.main.lowPriorityQueue.count, 0)
     }
     
+    func testLowAlertQueue() {
+        let alert = Alert(title: "Low", message: "Message", priorty: .low, alertActions: nil)
+        AlertCoordinator.main.enqueue(alert: alert)
+        XCTAssertEqual(AlertCoordinator.main.highPriorityQueue.count, 0)
+        XCTAssertEqual(AlertCoordinator.main.defaultPriorityQueue.count, 0)
+        XCTAssertEqual(AlertCoordinator.main.lowPriorityQueue.count, 1)
+    }
+    
+    func testResetOfAlertCoordinator() {
+        let alert = Alert(title: "Default", message: "Message", priorty: .medium, alertActions: nil)
+        AlertCoordinator.main.enqueue(alert: alert)
+        XCTAssertEqual(AlertCoordinator.main.defaultPriorityQueue.count, 1)
+        AlertCoordinator.main.reset()
+        XCTAssertEqual(AlertCoordinator.main.highPriorityQueue.count, 0)
+        XCTAssertEqual(AlertCoordinator.main.defaultPriorityQueue.count, 0)
+        XCTAssertEqual(AlertCoordinator.main.lowPriorityQueue.count, 0)
+    }
 }
